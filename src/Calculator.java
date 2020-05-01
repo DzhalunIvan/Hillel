f8ff7f6122eda77f9e40b7b9649d3c78c5003846
swild <swildua@gmail.com>
2020-05-01 15:49:34 +0300

    calculator added

---
+++ src/Calculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Please input first number");
+        Scanner scanner = new Scanner(System.in);
+        double x1 = scanner.nextDouble();
+        System.out.println("Please input second number");
+        double x2 = scanner.nextDouble();
+        double sum = x1+x2;
+        double diff = x1-x2;
+        double mult = x1*x2;
+        double div = x1/x2;
+        System.out.printf("Your \n Sum is %.3f\n Difference %.3f\n Multiplication %.3f\n Division %.3f",sum, diff, mult, div);
+
+    }
+}

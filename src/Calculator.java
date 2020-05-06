b6d76b844248c683a3570f3f44b5ff96df73dc13
swild <swildua@gmail.com>
2020-05-07 00:19:40 +0300

    calculator updated.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,27 +1,63 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
         double x1;
         double x2;
+        double sum;
+        double diff;
+        double multp;
+        double div;
         System.out.println("Please input first number");
         Scanner scanner = new Scanner(System.in);
-       if (!scanner.hasNextDouble()){
-           System.out.println("Wrong input! Please, enter numbers");
-           return;
-       }
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Wrong input! Please, try again");
+            scanner.next();
+        }
         x1 = scanner.nextDouble();
         System.out.println("Please input second number");
-        if (!scanner.hasNextDouble()){
-            System.out.println("Wrong input! Please, enter numbers");
-            return;
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Wrong input! Please, try again");
+            scanner.next();
         }
         x2 = scanner.nextDouble();
-        double sum = x1+x2;
-        double diff = x1-x2;
-        double mult = x1*x2;
-        double div = x1/x2;
-        System.out.printf("Your input is %.3f and %.3f \n Sum is %.3f\n Difference %.3f\n Multiplication %.3f\n Division %.3f",x1,x2,sum, diff, mult, div);
+        System.out.printf("You entered %f as a first number and %f as a second number.\n ", x1, x2);
+        System.out.println("Please, choose math operation: /, *,+,-");
 
+        while (!(scanner.hasNext("\\*") || scanner.hasNext("/") || scanner.hasNext("-") || scanner.hasNext("\\+"))) {
+            System.out.println("Wrong input! Try again.");
+            scanner.next();
+        }
+        String operand = scanner.next();
+        switch (operand) {
+            case "+": {
+                sum = x1 + x2;
+                System.out.printf("%f %s %f = %f\n", x1, operand, x2, sum);
+                break;
+            }
+            case "-": {
+                diff = x1 - x2;
+                System.out.printf("%f %s %f = %f\n", x1, operand, x2, diff);
+                break;
+            }
+            case "*": {
+                multp = x1 * x2;
+                System.out.printf("%f %s %f = %f\n", x1, operand, x2, multp);
+                break;
+            }
+            case "/": {
+                if (x2 == 0) {
+                    System.out.println("Division by zero is undefined");
+                    break;
+                }
+                div = x1 / x2;
+                System.out.printf("%f %s %f = %f\n", x1, operand, x2, div);
+                break;
+            }
+            default: {
+                System.out.println("You need to enter +, -, *, /!");
+            }
+        }
     }
 }
